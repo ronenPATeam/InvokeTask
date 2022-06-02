a83ce4670477d69c3ae7f0f2d699dfca7cc74310
Mateusz Majchrzak <Mateusz@pateam.co>
2022-06-02 12:29:21 +0200

    Removed unnecessary code. Added possibility to specify user and password for user

--- Class1.cs
+++ Class1.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net;
 using log4net;
 using System.IO;
-using System.Windows.Forms;
 using Newtonsoft.Json;
 using Direct.Shared;
 using System.Configuration;
@@ -17,9 +16,21 @@ namespace InvokeTask.Dll
     public class TaskInvoker
     {
         private static readonly ILog logArchitect = LogManager.GetLogger(Loggers.LibraryObjects);
+        internal static string _openAMUser;
+        internal static string _openAMPassword;
+
+        [DirectDom("Set OpenAM Credentials")]
+        [DirectDomMethod("Set OpenAM Credentials {username} {password}")]
+        [MethodDescriptionAttribute("Sets OpenAM credentials necessary to authenticate to invoke a task")]
+        public static void setOpenAMCredentials(string username, string password)
+        {
+            _openAMUser = username;
+            _openAMPassword = password;
+
+        }
 
         [DirectDom("Invoke")]
-        [DirectDomMethod("Invoke Solution ID{Text}, Workflow ID{WFID}, Priority{Number}, Input Data{Text}, Business Data{Text}")]
+        [DirectDomMethod("Invoke Solution ID {solutionId}, Workflow ID {workflowId}, Priority {priority}, Input Data {inputData}, Business Data {businessData}")]
         [MethodDescriptionAttribute("Creates a task with the information given in the type")]
         public static string invokeTask(string solutionID, string wfID, int priority,string data,string businessData)
         {
@@ -132,47 +143,6 @@ namespace InvokeTask.Dll
             return "Failed to invoke, check logs for more information";
         }
 
-        static protected string findWfID(string processName)
-        {
-            string URL;
-
-            try
-            {
-                URL = NiceRestHelpers.fetchFQDNfromConfig() + @"/RTServer/rest/nice/rti/ra/invocations/workflows";
-            }
-            catch
-            {
-                URL = ConfigurationManager.AppSettings["FQDN"] + @"/RTServer/rest/nice/rti/ra/invocations/workflows";
-            }
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
-
-            request.Method = "GET";
-            request.Accept = "application/json";
-            request.ContentType = "application/json";
-            request.MediaType = "application/json";
-            request.Headers.Add("Cookie", "iPlanetDirectoryPro=" + Get_Token.getToken());
-            try
-            {
-                WebResponse webResponse = request.GetResponse();
-                using (Stream webStream = webResponse.GetResponseStream() ?? Stream.Null)
-                using (StreamReader responseReader = new StreamReader(webStream))
-                {
-                    string response = responseReader.ReadToEnd();
-                    WFsAnswerRoot returnedAnswer = JsonConvert.DeserializeObject<WFsAnswerRoot>(response);
-                    foreach (Workflow wf in returnedAnswer.workflows)
-                    {
-                        if (wf.solutionDisplay == processName)
-                            return wf.id;
-                    }
-                    return "Workflow Not Found";
-                }
-            }
-            catch (Exception e)
-            {
-            }
-            return "Workflow Not Found. Please check log for more details";
-        }
-
         #region Invoke Task Support Classes
         protected class RequestMetaData
         {
@@ -242,44 +212,7 @@ namespace InvokeTask.Dll
         }
         #endregion
 
-        #region Find WF ID Support Classes
-        protected class Parameter
-        {
-            public string name { get; set; }
-            public string displayName { get; set; }
-            public string type { get; set; }
-            public string description { get; set; }
-            public bool list { get; set; }
-        }
 
-        protected class Parameters
-        {
-            public List<Parameter> parameters { get; set; }
-        }
-
-        protected class Definition
-        {
-            public string name { get; set; }
-            public string displayName { get; set; }
-            public string returnValueType { get; set; }
-            public object description { get; set; }
-            public Parameters parameters { get; set; }
-        }
-
-        protected class Workflow
-        {
-            public string id { get; set; }
-            public Definition definition { get; set; }
-            public string solutionId { get; set; }
-            public string solutionDisplay { get; set; }
-            public int version { get; set; }
-        }
-
-        protected class WFsAnswerRoot
-        {
-            public List<Workflow> workflows { get; set; }
-        }
-        #endregion
     }
     public class NiceRestHelpers
     {
@@ -314,8 +247,8 @@ namespace InvokeTask.Dll
             request.MediaType = "application/json";
             request.PreAuthenticate = true;
             request.AllowAutoRedirect = true;
-            request.Headers.Add("X-OpenAM-Username", "commonground");
-            request.Headers.Add("X-OpenAM-Password", "y4Ze%NFvgExgS^A2PF!R");
+            request.Headers.Add("X-OpenAM-Username", TaskInvoker._openAMUser);
+            request.Headers.Add("X-OpenAM-Password", TaskInvoker._openAMPassword);
 
             try
             {
@@ -365,220 +298,5 @@ namespace InvokeTask.Dll
             public string tokenId { get; set; }
             public string successUrl { get; set; }
         }
-    }
-    class Solution_Dproj_Mapping
-    {
-        #region Functions
-        public static string getDprojIdGeneral(string projectName)
-        {
-            string URL;
-            try
-            {
-                URL = NiceRestHelpers.fetchFQDNfromConfig() + @"/RTServer/rest/nice/rti/core/solution-assignments/solutions?active=true";
-            }
-            catch
-            {
-                URL = ConfigurationManager.AppSettings["FQDN"] + @"/RTServer/rest/nice/rti/core/solution-assignments/solutions?active=true";
-            }
-             HttpWebRequest request = null;
-            try
-            {
-                request = (HttpWebRequest)WebRequest.Create(URL);
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show("Could not parse URL string. Please make sure FQDN under AppSettings is configured in the client's root folder");
-                throw new Exception("Failed to create request");
-            }
-            request.Method = "GET";
-            request.Accept = "application/json";
-            request.ContentType = "application/json";
-            request.MediaType = "application/json";
-            request.Headers.Add("Cookie", "iPlanetDirectoryPro=" + Get_Token.getToken());
-            try
-            {
-                //if (Common_Ground.logArchitect.IsDebugEnabled)
-                //Common_Ground.logArchitect.Error("Common Ground- sending URI: " + request.RequestUri.ToString());
-                WebResponse webResponse = request.GetResponse();
-                using (Stream webStream = webResponse.GetResponseStream() ?? Stream.Null)
-                using (StreamReader responseReader = new StreamReader(webStream))
-                {
-                    string response = responseReader.ReadToEnd();
-                    ReturnedSolutionsList returnedSolutionsList = JsonConvert.DeserializeObject<ReturnedSolutionsList>(response);
-                    foreach (Solution_Dproj_Mapping.Solution solution in returnedSolutionsList.solutions)
-                    {
-                        if (solution.name == projectName)
-                            return solution.solutionId;
-                    }
-                    throw new Exception("Solution Not Found - Please verify package is assigned to the user");
-                }
-            }
-            catch (Exception e)
-            {
-            }
-            throw new Exception("Solution Not Found - Please verify package is assigned to the user");
-        }
-
-        public static string getDprojId(string projectName)
-        {
-            string URL = null;
-            try
-            {
-                if (Direct.Shared.Environment_Functions.TeamID == null)
-                {
-                    throw new Exception("No Team ID identified");
-                }
-                try
-                {
-                    URL = NiceRestHelpers.fetchFQDNfromConfig() + @"/RTServer/rest/nice/rti/core/solution-assignments/" +
-                    Direct.Shared.Environment_Functions.TeamID.Replace(@"\", "$$$");
-                }
-                catch
-                {
-                    URL = ConfigurationManager.AppSettings["FQDN"] + @"/RTServer/rest/nice/rti/core/solution-assignments/" +
-                    Direct.Shared.Environment_Functions.TeamID.Replace(@"\", "$$$");
-                }
-
-            }
-            catch (Exception e)
-            {
-                throw new Exception(e.ToString());
-            }
-            HttpWebRequest request = null;
-            try
-            {
-                request = (HttpWebRequest)WebRequest.Create(URL);
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show("Could not parse URL string. Please make sure FQDN under AppSettings is configured in the client's root folder");
-                throw new Exception("Failed to create request");
-            }
-            request.Method = "GET";
-            request.Accept = "application/json";
-            request.ContentType = "application/json";
-            request.MediaType = "application/json";
-            request.Headers.Add("Cookie", "iPlanetDirectoryPro=" + Get_Token.getToken());
-            try
-            {
-                //if (Common_Ground.logArchitect.IsDebugEnabled)
-                //Common_Ground.logArchitect.Error("Common Ground- sending URI: " + request.RequestUri.ToString());
-                WebResponse webResponse = request.GetResponse();
-                using (Stream webStream = webResponse.GetResponseStream() ?? Stream.Null)
-                using (StreamReader responseReader = new StreamReader(webStream))
-                {
-                    string response = responseReader.ReadToEnd();
-                    ReturnedAssignmentData assignedSolution = JsonConvert.DeserializeObject<ReturnedAssignmentData>(response);
-                    foreach (SelfSolution selfSolution in assignedSolution.selfSolutions)
-                    {
-                        if (selfSolution.solution.display == projectName)
-                            return selfSolution.solution.solutionId;
-                    }
-                    throw new Exception("Solution Not Found  - Please verify package is assigned to the user");
-                }
-            }
-            catch (Exception e)
-            {
-            }
-            throw new Exception("Solution Not Found - Please verify package is assigned to the user");
-        }
-
-        public static List<SelfSolution> getAssignedVersion()
-        {
-            string URL = null;
-            try
-            {
-                try
-                {
-                    URL = NiceRestHelpers.fetchFQDNfromConfig() + @"/RTServer/rest/nice/rti/core/solution-assignments/" +
-                    Direct.Shared.Environment_Functions.TeamID.Replace(@"\", "$$$");
-                }
-                catch
-                {
-                    URL = ConfigurationManager.AppSettings["FQDN"] + @"/RTServer/rest/nice/rti/core/solution-assignments/" +
-                    Direct.Shared.Environment_Functions.TeamID.Replace(@"\", "$$$");
-                }
-
-            }
-            catch (Exception e)
-            {
-                return new List<SelfSolution>();
-            }
-            HttpWebRequest request = null;
-            try
-            {
-                request = (HttpWebRequest)WebRequest.Create(URL);
-            }
-            catch (Exception e)
-            {
-                throw new Exception("Failed to create request");
-            }
-            request.Method = "GET";
-            request.Accept = "application/json";
-            request.ContentType = "application/json";
-            request.MediaType = "application/json";
-            request.Headers.Add("Cookie", "iPlanetDirectoryPro=" + Get_Token.getToken());
-            try
-            {
-                //if (Common_Ground.logArchitect.IsDebugEnabled)
-                //Common_Ground.logArchitect.Error("Common Ground- sending URI: " + request.RequestUri.ToString());
-                WebResponse webResponse = request.GetResponse();
-                using (Stream webStream = webResponse.GetResponseStream() ?? Stream.Null)
-                using (StreamReader responseReader = new StreamReader(webStream))
-                {
-                    string response = responseReader.ReadToEnd();
-                    ReturnedAssignmentData returnedData = JsonConvert.DeserializeObject<ReturnedAssignmentData>(response);
-                    return returnedData.selfSolutions;
-                }
-            }
-            catch (Exception e)
-            {
-            }
-            return new List<SelfSolution>();
-        }
-        #endregion
-
-        public class Version
-        {
-            public int versionNumber { get; set; }
-            public string description { get; set; }
-        }
-
-        public class Solution
-        {
-            public string solutionId { get; set; }
-            public string name { get; set; }
-            public List<Version> versions { get; set; }
-        }
-
-        public class ReturnedSolutionsList
-        {
-            public List<Solution> solutions { get; set; }
-        }
-    }
-
-    public class Solution
-    {
-        public string solutionId { get; set; }
-        public int version { get; set; }
-        public string display { get; set; }
-        public string notes { get; set; }
-    }
-
-    public class SelfSolution
-    {
-        public string teamId { get; set; }
-        public bool loadOnStartup { get; set; }
-        public object assignedOn { get; set; }
-        public string assignedBy { get; set; }
-        public Solution solution { get; set; }
-    }
-
-    public class ReturnedAssignmentData
-    {
-        public List<SelfSolution> selfSolutions { get; set; }
-        public List<object> parentSolutions { get; set; }
-        public bool viewable { get; set; }
-        public bool editable { get; set; }
-    }    
+    }  
 }
